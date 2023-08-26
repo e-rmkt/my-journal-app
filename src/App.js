f80278b281c0a6362874f105313ad02463b2d9de
Eliza Romeikat <eliza-romeikat@gmx.de>
2023-08-26 19:08:03 +0200

    append Form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import EntryForm from "./components/EntryForm/EntryForm";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <EntryForm />
       <Footer />
     </div>
   );
